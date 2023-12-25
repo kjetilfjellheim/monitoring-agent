c4915a2565951598348a51da39deee862146c6ae
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-12-25 15:36:12 +0100

    feat: Implemented config read
    Implemented functionality for parsing json files into the config domain
    model. Added tests for reading the config and verifying the imported
    data. The config model should after being read be considered immutable.
    
    No breaking changes,
    
    Resolves: None

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,162 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs;
+use tokio_cron_scheduler::{Job, JobScheduler};
+use std::error::Error;
 
-enum TypeSend<'a> {
-    Tcp(VerifyTcpRequest<'a>, Option<ServerCertificateType<'a>>, Option<ClientCertificateType<'a>>, VerifyTcp),      
-    Http(VerifyHttpRequest<'a>, Option<HttpsServerCertificateType<'a>>, Option<ServerCertificateType<'a>>, Option<ClientCertificateType<'a>>, VerifyHttp)   
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum MonitorType {
+    Tcp {
+        ip: String,
+        port: u32,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "serverCertificate")]
+        server_certificate: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "privateKey")]
+        private_key: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "privateKeyPassword")]
+        private_key_password: Option<String>,
+    },
+    Http {
+        url: String,
+        method: HttpMethod,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        body: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        headers: Option<HashMap<String, String>>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "httpsCertificate")]
+        https_certificate: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "serverCertificate")]
+        server_certificate: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "privateKey")]
+        private_key: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none", rename = "privateKeyPassword")]
+        private_key_password: Option<String>,
+    },
 }
 
-enum HttpMethod {
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum HttpMethod {
     Get,
     Post,
     Put,
     Delete,
     Option,
-    Head
+    Head,
 }
 
-struct HttpsServerCertificateType<'a> {
-    certificate: &'a str
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Monitor {
+    schedule: String,
+    monitor: MonitorType,
 }
 
-struct ServerCertificateType<'a> {
-    certificate: &'a str
+#[derive(Debug, PartialEq)]
+pub struct MonitoringConfig {
+    input: String,
+    monitors: Vec<Monitor>,
 }
 
-struct ClientCertificateType<'a> {
-    certificate: &'a str,
-    password: &'a str
-}
+impl MonitoringConfig {
+    pub fn new(input: &str) -> MonitoringConfig {
+        let monitor_data: String = MonitoringConfig::get_monitor_data(input);
+        let monitors: Vec<Monitor> = MonitoringConfig::get_monitor_config(monitor_data.as_str()).unwrap();
+        MonitoringConfig {
+            input: input.to_string(),
+            monitors,
+        }
+    }
 
-struct VerifyTcpRequest<'a> {
-    ip: &'a str,
-    port: i32
-}
+    fn get_monitor_data(path: &str) -> String {
+        fs::read_to_string(path).unwrap()
+    }
 
-struct VerifyHttpRequest<'a> {
-    url: &'a str,
-    method: HttpMethod,
-    body: &'a str,
-    headers: HashMap<&'a str, &'a str>
+    fn get_monitor_config(data: &str) -> Result<Vec<Monitor>, serde_json::Error> {
+        serde_json::from_str(data)
+    }
 }
-
-struct VerifyTcp {
-}
-
-struct VerifyHttp {
-    http_code: i32
-}
-
-enum TlsType<'a> {
-    Tls(&'a str),                       // Server certificate
-    Mtls(&'a str, &'a str, &'a str)     // Server certificate, Client key, Client password
-}
-
-struct Monitor<'a> {
-    test_type: TypeSend<'a>,
-    tls: Option<TlsType<'a>>
-}
-
 
 fn main() {
+    MonitoringConfig::new("resources/test/test_simple.json");
+}
 
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_simple_tcp_file() {
+        let monitoring: MonitoringConfig = MonitoringConfig::new("resources/test/test_simple_tcp.json");
+        assert_eq!("0 0 0 0 0 0 0".to_string(), monitoring.monitors[0].schedule);
+        assert_eq!(1, monitoring.monitors.len());
+        let monitor = monitoring.monitors[0].monitor.clone();
+        assert_eq!(
+            monitor,
+            MonitorType::Tcp {
+                ip: "192.168.1.1".to_string(),
+                port: 8080,
+                server_certificate: Some("server_cert1".to_string()),
+                private_key: Some("privatekey1".to_string()),
+                private_key_password: Some("privatekeypasswd1".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_simple_http_file() {
+        let monitoring: MonitoringConfig = MonitoringConfig::new("resources/test/test_simple_http.json");
+        assert_eq!("1 2 3 4 5 6 7".to_string(), monitoring.monitors[0].schedule);
+        assert_eq!(1, monitoring.monitors.len());
+        let monitor = monitoring.monitors[0].monitor.clone();
+        assert_eq!(
+            monitor,
+            MonitorType::Http {
+                url: "https://post.com".to_string(),
+                body: Some("body".to_string()),
+                method: HttpMethod::Post,
+                headers: Some(HashMap::new()),
+                https_certificate: Some("httpscert2".to_string()),
+                server_certificate: Some("server_cert2".to_string()),
+                private_key: Some("privatekey2".to_string()),
+                private_key_password: Some("privatekeypasswd2".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_multiple_file() {
+        let monitoring: MonitoringConfig = MonitoringConfig::new("resources/test/test_multiple.json");
+        assert_eq!("0 0 0 0 0 0 0".to_string(), monitoring.monitors[0].schedule);
+        assert_eq!("0 0 0 0 0 0 1".to_string(), monitoring.monitors[1].schedule);
+        assert_eq!(2, monitoring.monitors.len());
+        let monitor = monitoring.monitors[0].monitor.clone();
+        assert_eq!(
+            monitor,
+            MonitorType::Tcp {
+                ip: "192.168.1.1".to_string(),
+                port: 8080,
+                server_certificate: None,
+                private_key: None,
+                private_key_password: None
+            }
+        );
+        let monitor = monitoring.monitors[1].monitor.clone();
+        assert_eq!(
+            monitor,
+            MonitorType::Http {
+                url: "https://test.com".to_string(),
+                body: None,
+                method: HttpMethod::Get,
+                headers: None,
+                https_certificate: None,
+                server_certificate: None,
+                private_key: None,
+                private_key_password: None
+            }
+        );
+    }
 }
