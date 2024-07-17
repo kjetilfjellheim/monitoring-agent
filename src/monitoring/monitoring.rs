373e927c465c57b4dc5fea7f24c3b1d6a8a6a8b7
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2024-07-17 17:57:10 +0200

    refactor: Refactor the code
    Refactor the code to make it more readable and maintainable. It changes
    the structure of the code without changing its external behavior.
    Removed an issue with the code that was causing a bug with the
    httpmonitor. The issue was that the httpmonitor did not correctly
    send the request to the server. The issue was fixed by removing the
    tokio runtime so only one tokio runtime is running at a time.
    
    Breaking changes: None
    
    Resolves: None

--- src/monitoring/monitoring.rs
+++ src/monitoring/monitoring.rs
@@ -17,7 +17,8 @@ use crate::config::HttpMethod;
  */
 pub struct MonitoringService {
     scheduler: Option<JobScheduler>,
-    status: Vec<Box<dyn MonitorTrait>>
+    tcp_monitors: Vec<TcpMonitor>,
+    http_monitors: Vec<HttpMonitor>,
 }
 
 impl MonitoringService {
@@ -27,7 +28,8 @@ impl MonitoringService {
     pub fn new() -> MonitoringService {    
         MonitoringService {
             scheduler: None,
-            status: Vec::new()
+            tcp_monitors: Vec::new(),
+            http_monitors: Vec::new(),
         }
     }
 
@@ -55,7 +57,7 @@ impl MonitoringService {
         /*
          * Block the main thread until the scheduling is done.
          */
-        match tokio::runtime::Builder::new_current_thread().enable_all().build() {
+        match tokio::runtime::Builder::new_multi_thread().enable_all().build() {
             Ok(runtime) => {
                 runtime.block_on(future_scheduling)?;
             },
@@ -92,10 +94,10 @@ impl MonitoringService {
             let monitor_type = monitor.monitor.clone();
             let job = match monitor_type {
                 crate::config::MonitorType::Tcp{host, port} => {
-                    self.get_tcp_monitor_job(monitor.schedule.as_str(), monitor.name.as_str(), host.as_str(), &port)?                  
+                    self.get_tcp_monitor_job(monitor.schedule.as_str(), monitor.name.as_str(), host.as_str(), &port).await?                                     
                 },
                 crate::config::MonitorType::Http{url, method, body, headers} => {
-                    self.get_http_monitor_job(monitor.schedule.as_str(), monitor.name.as_str(), url.as_str(), method, body, headers)?
+                    self.get_http_monitor_job(monitor.schedule.as_str(), monitor.name.as_str(), url.as_str(), &method, &body, &headers).await?
                 },
                 _ => {
                     return Err(ApplicationError::new("Unsupported monitor type"));
@@ -114,8 +116,49 @@ impl MonitoringService {
                 return Err(ApplicationError::new(format!("Could not start scheduler: {}", err).as_str()));
             }
         }        
-        loop {
-            tokio::time::sleep(Duration::from_secs(20000)).await;
+        loop {    
+            for tcp_monitor in self.tcp_monitors.iter() {
+                match tcp_monitor.status.lock() {
+                    Ok(lock) => {
+                        match &*lock {
+                            MonitorStatus::Ok => {
+                                println!("Gen {}: Ok", tcp_monitor.name);
+                            },
+                            MonitorStatus::Unknown => {
+                                println!("Gen {}: Unknown", tcp_monitor.name);
+                            },
+                            MonitorStatus::Error { message } => {
+                                println!("Gen {}: Error: {}", tcp_monitor.name, message);
+                            }
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("Error getting monitor status: {:?}", err);
+                    }
+                }
+            }
+            for http_monitor in self.http_monitors.iter() {
+                match http_monitor.status.lock() {
+                    Ok(lock) => {
+                        match &*lock {
+                            MonitorStatus::Ok => {
+                                println!("Gen {}: Ok", http_monitor.name);
+                            },
+                            MonitorStatus::Unknown => {
+                                println!("Gen {}: Unknown", http_monitor.name);
+                            },
+                            MonitorStatus::Error { message } => {
+                                println!("Gen {}: Error: {}", http_monitor.name, message);
+                            }
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("Error getting monitor status: {:?}", err);
+                    }
+                }
+            }
+
+            tokio::time::sleep(Duration::from_secs(20)).await;
         }
     }
 
@@ -150,23 +193,73 @@ impl MonitoringService {
      * 
      * @throws ApplicationError: If the job fails to be created.
      */
-    fn get_tcp_monitor_job(&mut self, schedule: &str, name: &str, host: &str, port: &u16) -> Result<Job, ApplicationError> {  
-        let mut tcp_monitor = TcpMonitor::new(host, port, name);
-        self.status.push(Box::new(tcp_monitor.clone()));
-        tcp_monitor.get_job(schedule)   
+    async fn get_tcp_monitor_job(&mut self, schedule: &str, name: &str, host: &str, port: &u16) -> Result<Job, ApplicationError> {  
+        let tcp_monitor = TcpMonitor::new(host, port, name);
+        self.tcp_monitors.push(tcp_monitor.clone());
+        let name = name.to_string().clone();
+        let host = host.to_string().clone();
+        let port = port.clone();
+        match Job::new_async(schedule, move |a,b| {
+            Box::pin({
+            let mut moved_tcp_monitor = tcp_monitor.clone();
+            let host = host.to_string().clone();
+            let name = name.to_string().clone();
+            async move {
+                match moved_tcp_monitor.check(&host, &port).await {
+                    Ok(_) => {
+                        
+                    },
+                    Err(err) => {
+                        eprintln!("{}: Error: {}", &name, err.message);
+                    }
+                }
+            }
+            })
+        }) {
+            Ok(job) => {
+                Ok(job)
+            },
+            Err(err) => {
+                Err(ApplicationError::new(format!("Could not create job: {}", err).as_str()))
+            }
+        }
     }
 
-    fn get_http_monitor_job(&mut self, schedule: &str, name: &str, url: &str, method: HttpMethod, body:Option<String>, headers:Option<std::collections::HashMap<String, String>>) -> Result<Job, ApplicationError> {  
-        let mut http_monitor = HttpMonitor::new(url, method, body, headers, &name);
-        self.status.push(Box::new(http_monitor.clone()));
-        http_monitor.get_job(schedule)   
+    async fn get_http_monitor_job(&mut self, schedule: &str, name: &str, url: &str, method: &HttpMethod, body: &Option<String>, headers: &Option<std::collections::HashMap<String, String>>) -> Result<Job, ApplicationError> {  
+        let http_monitor = HttpMonitor::new(url, &method, body, headers, &name);
+        self.http_monitors.push(http_monitor.clone());
+        let name = name.to_string().clone();
+        let url = url.to_string().clone();
+        let method = method.clone();
+        let headers = headers.clone();
+        let body = body.clone();
+        match Job::new_async(schedule, move |a,b| {
+            Box::pin({
+            let mut moved_http_monitor = http_monitor.clone();
+            let name = name.to_string().clone();
+            let url = url.to_string().clone();
+            let method = method.clone();
+            let headers = headers.clone();
+            let body = body.clone();            
+            async move {
+                match moved_http_monitor.check(&method, &url, &headers, &body).await {
+                    Ok(_) => {
+                    },
+                    Err(err) => {
+                        eprintln!("{}: Error: {}", &name, err.message);
+                    }
+                }
+            }
+            })
+        }) {
+            Ok(job) => {
+                Ok(job)
+            },
+            Err(err) => {
+                Err(ApplicationError::new(format!("Could not create job: {}", err).as_str()))
+            }
+        }
     }
-}
-
-pub trait MonitorTrait {
-    fn get_job(&mut self, schedule: &str) -> Result<Job, ApplicationError>;
-    fn get_status(&self) -> MonitorStatus;
-    fn get_name(&self) -> String;
 }
 
 #[derive(Debug, Clone)]
