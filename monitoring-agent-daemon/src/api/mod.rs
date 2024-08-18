eca52e2df956892a98fd63f4531cb4137a554ca5
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2024-08-18 19:22:57 +0200

    feat:Issue-137:Add ping status check (#138)
    Adds a new endpoint ad / which returns a simple response. This is used
    in the UI to check if the agent is running and if the set url is
    correct.
    
    Breaking changes: None
    
    Resolves: #137

--- monitoring-agent-daemon/src/api/mod.rs
+++ monitoring-agent-daemon/src/api/mod.rs
@@ -18,12 +18,15 @@ mod loadavg;
 mod process;
 mod monitor;
 mod common;
+mod ping;
 
 pub use crate::api::meminfo::get_current_meminfo;
 pub use crate::api::cpuinfo::get_current_cpuinfo;
 pub use crate::api::loadavg::get_current_loadavg;
 pub use crate::api::process::{get_processes, get_process, get_threads, get_current_statm};
 pub use crate::api::monitor::get_monitor_status;
+pub use crate::api::ping::get_ping;
+
 
 #[allow(clippy::module_name_repetitions)]
 pub use crate::api::state::StateApi;
