85d3fbd61ae0e63c53ee5393f2930accb99c619e
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2024-08-18 16:28:02 +0200

    feat:Issue-126:Implement settings page
    Implements a settings page where it's possible to add. remove or test
    servers to get monitors from. Also changes the background to dark for
    better visibility.
    
    Breaking chnages: None
    
    Resolves: #126

--- monitoring-agent-ui/src/components/Monitors.vue
+++ monitoring-agent-ui/src/components/Monitors.vue
@@ -1,101 +1,110 @@
 <script setup>
-    import { ref } from 'vue';
-    const data = ref(null);
-    fetch('http://localhost:64999/monitors/status')
+import { ref } from 'vue';
+const monitors = ref(null);
+const URL_NAME = 'apiUrls';
+monitors.value = [];
+let currentUrls = localStorage.getItem(URL_NAME);
+let urls = JSON.parse(currentUrls);
+for (let url of urls) {
+    fetch(url + "/monitors/status")
         .then(response => response.json())
-        .then(response => data.value = response)
+        .then(response => monitors.value.push(...response))
         .catch(error => alert(error));
+}                
 </script>
 
 <script>
 export default {
     data() {
         return {
-            showErrorStatus: true, 
-            showOkStatus: true, 
-        };
-    },
-    methods: {
-        handleCheckboxChange() {
-            // This method will be called when the checkbox state changes
-            console.log("Checkbox state changed. Checked:", this.isChecked);
-        },
-    },
+            showErrorStatus: true,
+            showOkStatus: true
+        }
+    }
 };
 </script>
 
 <template>
-    <div class="form-check toolbar-item">
-        <input class="form-check-input" type="checkbox" id="showOErrorStatus" v-model="showErrorStatus">
-        <label class="form-check-label" for="showOErrorStatus">
-            Show Error status
-        </label>
-    </div>
-    <div class="form-check toolbar-item">
-        <input class="form-check-input" type="checkbox" id="showOkStatus" v-model="showOkStatus">
-        <label class="form-check-label" for="showOkStatus">
-            Show Ok status
-        </label>
-    </div>
-    <div v-if="data">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <input class="form-check-input toolbar-item" type="checkbox" id="showOErrorStatus"
+                        v-model="showErrorStatus"></input>
+                    <label class="form-check-label text-light" for="showOErrorStatus">Show Error</label>
+                </li>
+                <li class="nav-item">
+                    <input class="form-check-input toolbar-item" type="checkbox" id="showOkStatus" v-model="showOkStatus">
+                    <label class="form-check-label text-light" for="showOkStatus">Show Ok</label>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div v-if="monitors">
         <div class="container-fluid">
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-4 row-cols-lg-6">
-                <template v-for="monitor in data">  
+                <template v-for="monitor in monitors">
 
-                <div class="col" v-if="(monitor.status === 'Ok' && showOkStatus) || (monitor.status === 'Error' && showErrorStatus)">
-                    <div class="card">
-                        <div v-if="monitor.status === 'Ok'" class="card-header bg-success">
-                            <h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-shield-check" viewBox="0 0 16 16">
-                                    <path
-                                        d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
-                                    <path
-                                        d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
-                                </svg>
-                                {{ monitor.name }}
+                    <div class="col"
+                        v-if="(monitor.status === 'Ok' && showOkStatus) || (monitor.status === 'Error' && showErrorStatus)">
+                        <div class="card">
+                            <div v-if="monitor.status === 'Ok'" class="card-header bg-success">
+                                <h5>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-shield-check" viewBox="0 0 16 16">
+                                        <path
+                                            d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
+                                        <path
+                                            d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
+                                    </svg>
+                                    {{ monitor.name }}
 
-                            </h5>
-                        </div>
-                        <div v-else="monitor.status === 'Ok'" class="card-header bg-danger">
-                            <h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-shield-x" viewBox="0 0 16 16">
-                                    <path
-                                        d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
-                                    <path
-                                        d="M6.146 5.146a.5.5 0 0 1 .708 0L8 6.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 7l1.147 1.146a.5.5 0 0 1-.708.708L8 7.707 6.854 8.854a.5.5 0 1 1-.708-.708L7.293 7 6.146 5.854a.5.5 0 0 1 0-.708" />
-                                </svg>
-                                {{ monitor.name }}
-                            </h5>
-                        </div>
-                        <div class="card-body">
-                            <dl class="row">
-                                <dt class="col-sm-5 small bg-light no-margin">Current status</dt>
-                                <dd class="col-sm-7 small text-truncate bg-light no-margin">{{ monitor.status }}</dd>
-                                <dt class="col-sm-5 small no-margin">Server</dt>
-                                <dd class="col-sm-7 small text-truncate no-margin">Localhost</dd>
-                                <dt class="col-sm-5 small no-margin" v-if="monitor.lastSuccessfulTime != null">Last
-                                    successful time</dt>
-                                <dd class="col-sm-7 small no-margin text-truncate"
-                                    v-if="monitor.lastSuccessfulTime != null">{{ monitor.lastSuccessfulTime }}</dd>
-                                <dt class="col-sm-5 small no-margin" v-if="monitor.lastErrorTime != null">Last eror time
-                                </dt>
-                                <dd class="col-sm-7 small no-margin text-truncate" v-if="monitor.lastErrorTime != null">
-                                    {{ monitor.lastErrorTime }}</dd>
-                                <dt class="col-sm-12 small no-margin" v-if="monitor.lastError != null">Last error</dt>
-                                <dd class="col-sm-12 small no-margin xtext-truncate" v-if="monitor.lastError != null"
-                                    :key="monitor._id" data-bs-toggle="tooltip" data-bs-placement="top"
-                                    v-bind:title="monitor.lastError">{{ monitor.lastError }}</dd>
-                            </dl>
+                                </h5>
+                            </div>
+                            <div v-else="monitor.status === 'Ok'" class="card-header bg-danger">
+                                <h5>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-shield-x" viewBox="0 0 16 16">
+                                        <path
+                                            d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
+                                        <path
+                                            d="M6.146 5.146a.5.5 0 0 1 .708 0L8 6.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 7l1.147 1.146a.5.5 0 0 1-.708.708L8 7.707 6.854 8.854a.5.5 0 1 1-.708-.708L7.293 7 6.146 5.854a.5.5 0 0 1 0-.708" />
+                                    </svg>
+                                    {{ monitor.name }}
+                                </h5>
+                            </div>
+                            <div class="card-body">
+                                <dl class="row">
+                                    <dt class="col-sm-5 small bg-light no-margin">Current status</dt>
+                                    <dd class="col-sm-7 small text-truncate bg-light no-margin">{{ monitor.status }}
+                                    </dd>
+                                    <dt class="col-sm-5 small no-margin">Server</dt>
+                                    <dd class="col-sm-7 small text-truncate no-margin">Localhost</dd>
+                                    <dt class="col-sm-5 small no-margin" v-if="monitor.lastSuccessfulTime != null">Last
+                                        successful time</dt>
+                                    <dd class="col-sm-7 small no-margin text-truncate"
+                                        v-if="monitor.lastSuccessfulTime != null">{{ monitor.lastSuccessfulTime }}</dd>
+                                    <dt class="col-sm-5 small no-margin" v-if="monitor.lastErrorTime != null">Last eror
+                                        time
+                                    </dt>
+                                    <dd class="col-sm-7 small no-margin text-truncate"
+                                        v-if="monitor.lastErrorTime != null">
+                                        {{ monitor.lastErrorTime }}</dd>
+                                    <dt class="col-sm-12 small no-margin" v-if="monitor.lastError != null">Last error
+                                    </dt>
+                                    <dd class="col-sm-12 small no-margin xtext-truncate"
+                                        v-if="monitor.lastError != null" :key="monitor._id" data-bs-toggle="tooltip"
+                                        data-bs-placement="top" v-bind:title="monitor.lastError">{{ monitor.lastError }}
+                                    </dd>
+                                </dl>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </template>
             </div>
         </div>
     </div>
-    <div v-if="!data" class="text-center">
+    <div v-if="!monitors" class="text-center">
         <div class="spinner-border spinner-border-sm"></div>
     </div>
 </template>
@@ -117,5 +126,6 @@ export default {
 
 .toolbar-item {
     margin-left: 10px;
+    margin-right: 5px;
 }
 </style>
--- monitoring-agent-ui/src/components/Settings.vue
+++ monitoring-agent-ui/src/components/Settings.vue
@@ -1,13 +1,88 @@
 <script>
+const URL_NAME = 'apiUrls';
+
 export default {
   data() {
     return {
-      count: 0
-    }
+      urls: this.getUrls(),
+      new_url: null,
+      remove_url: null
+    };
+  },
+  methods: {
+    addUrl(url) {
+      this.urls = this.getUrls();
+      this.urls.push(url);
+      this.saveUrls(this.urls);
+    },
+    getUrls() {
+      let currentUrls = localStorage.getItem(URL_NAME);
+      if (!currentUrls) {
+        currentUrls = [];
+        this.saveUrls(currentUrls);
+      }
+      let urls = JSON.parse(currentUrls);
+      return urls;
+    },
+    saveUrls(urls) {
+      localStorage.setItem(URL_NAME, JSON.stringify(urls));
+    },
+    removeUrl(url) {
+      let urls = this.getUrls();
+      urls = urls.filter(u => u !== url);
+      this.saveUrls(urls);
+      this.urls = this.getUrls();
+    },
+    test(url) {
+      fetch(url + "/monitors/status")
+        .then(response => response.json())
+        .then(response => alert("Got success response from " + url))
+        .catch(error => alert(error));
   }
 }
+}
 </script>
-
 <template>
-  Settings
+  <div class="container">
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
+            <form class="d-flex">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" aria-describedby="basic-addon2" v-model="new_url" />
+                <div class="input-group-append">
+                  <button class="btn btn-primary" @click="addUrl(new_url)">Add</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <table class="table table-striped table-dark">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">URL</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="url in urls" id="{{ url }}">
+            <th scope="row">1</th>
+            <td><input type="text" class="form-control" aria-describedby="basic-addon2" v-bind:value="url" /></td>
+            <td><button class="btn btn-danger function" @click="removeUrl(url)">Remove</button><button class="btn btn-info function" @click="test(url)">Test</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .function {
+    margin-right: 5px;
+  }
+</style>
