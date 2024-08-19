01535a218bb246380aba00536eda425848814ecf
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2024-08-19 18:26:21 +0200

    feat:Issue-155:Implement first version of the process ui
    Implements first version of the process ui. Includes filtering and the
    ability refresh the data.
    
    Breaking changes: None
    
    Resolves: #155

--- monitoring-agent-ui/src/components/Processes.vue
+++ monitoring-agent-ui/src/components/Processes.vue
@@ -1,104 +1,160 @@
-<script setup>
-import { ref } from 'vue';
-const processes = ref(null);
-const URL_NAME = 'apiUrls';
-processes.value = [];
-let currentUrls = localStorage.getItem(URL_NAME);
-let urls = JSON.parse(currentUrls);
-for (let url of urls) {
-    fetch(url + "/processes")
-        .then(response => response.json())
-        .then(json => {
-            json.forEach(element => {
-                element.url = url;
-            });
-            processes.value.push(...json)
-        })
-        .catch(error => alert(error));
-}                
-</script>
 <script>
+import { ref } from 'vue';
+
+
 export default {
     data() {
         return {
-            search: ""
+            searchServer: "",
+            searchPid: "",
+            searchParent: "",
+            searchName: "",
+            searchUmask: "",
+            searchState: "",
+            searchThreads: "",
+            processes: null
         }
     },
-    methods: {
+    mounted() {
+        this.refreshProcesses();        
+    },
+    methods: {        
+        refreshProcesses() {
+            const processes = ref(null);     
+            const URL_NAME = 'apiUrls';           
+            processes.value = [];
+            let currentUrls = localStorage.getItem(URL_NAME);
+            let urls = JSON.parse(currentUrls);
+            for (let url of urls) {
+                fetch(url + "/processes")
+                    .then(response => response.json())
+                    .then(json => {
+                        json.forEach(element => {
+                            element.url = url;
+                            if (element.name === undefined) {
+                                element.name = "";
+                            }
+                        });
+                        processes.value.push(...json)
+                    })
+                    .catch(error => console.error('Error:', error));
+            }
+            this.processes = processes.value;
+        },
         filter(process) {
-            return this.check_includes(process.url, this.search) || 
-                this.check_includes(process.pid.toString(), this.search) || 
-                this.check_includes(process.parentPid.toString(), this.search) || 
-                this.check_includes(process.name, this.search) ||
-                process.umask.includes(this.search) || 
-                process.processState.includes(this.search) || 
-                process.numThreads.toString().includes(this.search) || 
-                process.groups.join(", ").includes(this.search);            
+            return this.checkAllNull() ||
+                this.check_includes(process.url, this.searchServer) &&
+                this.check_includes(process.pid.toString(), this.searchPid) &&
+                this.check_includes(process.parentPid.toString(), this.searchParent) &&
+                this.check_includes(process.name, this.searchName) &&
+                this.check_includes(process.umask, this.searchUmask) &&
+                this.check_includes(process.processState, this.searchState) &&
+                this.check_includes(process.numThreads.toString(), this.searchThreads);
         },
         check_includes(value, search) {
             if (value === undefined) {
-                return false;
+                return true;
+            }
+            if (search === "") {
+                return true;
             }
             return value.includes(search);
+        },
+        checkAllNull() {
+            return this.searchServer === "" &&
+                this.searchPid === "" &&
+                this.searchParent === "" &&
+                this.searchName === "" &&
+                this.searchUmask === "" &&
+                this.searchState === "" &&
+                this.searchThreads === "";
         }
     }
 };
 </script>
 <template>
-    <div class="container" v-if="processes">
-      <div class="row">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-          <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">      
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-                <form class="d-flex">
-                    <div class="form-floating">
-                        <input id="searchInput" type="text" class="form-control" aria-describedby="basic-addon2" v-model="search" size="50" />
-                        <label for="searchInput">Search</label>
-                    </div>
-                </form>
-            </div>
-          </div>
-        </nav>
-        <table class="table table-striped table-dark">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Server</th>
-              <th scope="col">Pid</th>
-              <th scope="col">Parent</th>
-              <th scope="col">Name</th>
-              <th scope="col">Umask</th>
-              <th scope="col">State</th>
-              <th scope="col">Threads</th>
-              <th scope="col">Groups</th>
-              <th scope="col">Functions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-for="process in processes">
-                <tr v-if="filter(process)">
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.url }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.pid }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.parentPid }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.name }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.umask }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.processState }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.numThreads }}</label></td>
-                    <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.groups.join(", ") }}</label></td>
-                    <td class="vertical-align-middle">
-                        <button class="btn btn-info btn-sm tools">Details</button>
-                        <button class="btn btn-info btn-sm tools" disabled>Threads</button>
-                    </td>
-                </tr>
-            </template>
-          </tbody>
-        </table>
-      </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <button class="btn btn-info small" @click="this.refreshProcesses()">Refresh</button>
+                </li>                
+            </ul>
+        </div>
+    </nav>    
+    <div class="container-fluid" v-if="processes">
+        <div class="row">
+            <table class="table table-striped table-dark table-responsive">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Server</th>
+                        <th scope="col">Pid</th>
+                        <th scope="col">Parent</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Umask</th>
+                        <th scope="col">State</th>
+                        <th scope="col">Threads</th>
+                        <th scope="col">Memory</th>
+                        <th scope="col">Cpu</th>
+                    </tr>
+                    <tr>
+                        <th scope="col"><input id="idSearchServer" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchServer" minlength="10" maxLength="10"
+                                size="10" /></th>
+                        <th scope="col"><input id="idSearchPid" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchPid" minlength="5" maxLength="5"
+                                size="5" /></th>
+                        <th scope="col"><input id="idSearchParent" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchParent" minlength="5" maxLength="5"
+                                size="5" /></th>
+                        <th scope="col"><input id="idSearchName" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchName" minlength="16" maxLength="16"
+                                size="16" /></th>
+                        <th scope="col"><input id="idSearchUmask" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchUmask" minlength="10" maxLength="10"
+                                size="10" /></th>
+                        <th scope="col"><input id="isSearchState" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchState" minlength="10" maxLength="10"
+                                size="10" /></th>
+                        <th scope="col"><input id="isSearchThreads" type="text" class="form-control"
+                                aria-describedby="basic-addon2" v-model="searchThreads" minlength="3" maxLength="3"
+                                size="3" /></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template v-for="process in processes">
+                        <tr v-if="filter(process)">
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.url
+                                    }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.pid
+                                    }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{
+                                    process.parentPid }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{ process.name
+                                    }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{
+                                    process.umask }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{
+                                    process.processState }}</label></td>
+                            <td class="vertical-align-middle"><label class="form-check-label text-light">{{
+                                    process.numThreads }}</label></td>
+                            <td class="vertical-align-middle"><input class="btn btn-info btn-sm" type="button"
+                                    value="Get" disabled /></td>
+                            <td class="vertical-align-middle"><input class="btn btn-info btn-sm" type="button"
+                                    value="Get" disabled /></td>
+                        </tr>
+                    </template>
+                </tbody>
+            </table>
+        </div>
     </div>
     <div v-if="!processes" class="text-center">
         <div class="spinner-border spinner-border-sm"></div>
-    </div>    
-  </template>
+    </div>
+
+</template>
 
 <style scoped>
 .col {
@@ -122,5 +178,10 @@ export default {
 
 .tools {
     margin-right: 4px;
+}
+
+.toolbar-item {
+    margin-left: 10px;
+    margin-right: 5px;
 }
 </style>
